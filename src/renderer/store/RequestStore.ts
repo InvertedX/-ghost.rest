cbb4ef231767b1075d0765520fd096dd1e8cad50
Sarath kumar <sarathkumars677@gmail.com>
2018-12-10 23:43:33 +0530

    Mobx Request store and server handler store for render process

--- src/renderer/store/RequestStore.ts
+++ src/renderer/store/RequestStore.ts
@@ -1,15 +1,22 @@
 import { action, observable } from "mobx";
+import { IRequest, IResponse } from "../../shared/models";
 import { AbstractStore } from "./AbstractStore";
 class RequestStore extends AbstractStore {
-  @observable public value = 0;
+  @observable public requests:IRequest[] = [];
+  @observable public responses:IResponse[] = [];
 
   constructor() {
     super();
   }
 
   @action
-  public handleCounterValueReply(arg:number) {
-    this.value = arg;
+  public addRequest(request:any) {
+    this.requests = [...this.requests, request];
+  }
+
+  @action
+  public addResponse(response:any) {
+    this.responses = [...this.responses, response];
   }
 }
 
---
+++ src/renderer/store/ServerHandlerStore.ts
@@ -0,0 +1,30 @@
+import { action, observable } from "mobx";
+import { R2M_START_SERVER, R2M_STOP_SEVER } from "../../shared/ipc.events";
+import { IResponse, IServerOptions } from "../../shared/models";
+import { AbstractStore } from "./AbstractStore";
+
+class ServerHandlerStore extends AbstractStore {
+  @observable public currentTarget?:IServerOptions | null = null;
+  @observable public responses:IResponse[] = [];
+
+  constructor() {
+    super();
+  }
+
+  @action
+  public setTargetServer(target:IServerOptions) {
+    this.currentTarget = target;
+  }
+
+  @action
+  public startServer() {
+    this.sendR2m(R2M_START_SERVER, this.currentTarget);
+  }
+
+  @action
+  public stopServer(target:IServerOptions) {
+    this.sendR2m(R2M_STOP_SEVER, {});
+  }
+}
+
+export { ServerHandlerStore };
