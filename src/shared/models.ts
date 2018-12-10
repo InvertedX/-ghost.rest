01b078b4931dcf6b6469bd73b99dfe2afc1ca7ee
Sarath kumar <sarathkumars677@gmail.com>
2018-12-10 23:42:40 +0530

    Moving models

---
+++ src/shared/models.ts
@@ -0,0 +1,21 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import { Request, Response } from "express";
+
+export interface IRequest extends Request {
+  id:string;
+  size:string;
+  method:string;
+  status:number;
+  url:string;
+}
+export interface IResponse extends Response {
+  headers:string[];
+  id:string;
+  method:string;
+  size:string;
+  url:string;
+}
+export interface IServerOptions {
+  url:string;
+  port:number;
+}
